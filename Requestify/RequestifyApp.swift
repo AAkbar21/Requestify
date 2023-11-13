f92ca5d73a2663365f45fac92256d86781d20ed2
Abdullah Akbar <Akbar.a@etoncollege.org.uk>
2023-11-13 12:35:15 +0000

    Initial Commit

---
+++ Requestify/RequestifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestifyApp.swift
+//  Requestify
+//
+//  Created by Akbar, Abdullah (RCH) on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RequestifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
